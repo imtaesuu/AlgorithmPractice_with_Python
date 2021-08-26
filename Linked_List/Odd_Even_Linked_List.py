e7c9e4f1e09a0c5e7fc1f05b30a670db2fab9825
김태수 <xotn0194@naver.com>
2021-08-26 14:31:56 +0000

    Let's solve the linked list problem

--- Linked_List/Odd_Even_Linked_List.py
+++ Linked_List/Odd_Even_Linked_List.py
@@ -1,16 +1,35 @@
 from Linked_List import ListNode
 
+##### my code #####
+##### Runtime 36ms, Memory 16.1MB #####
+# class leetcode_328:
+#     def oddEvenList(self, head: ListNode) -> ListNode:
+#         if head is None:
+#             return head
+#         odd, even = head, head.next
+
+#         while head and head.next:
+#             head.next, head = head.next.next, head.next
+            
+#         start = odd       
+#         while start and start.next:
+#             start = start.next
+#         start.next = even
+#         return odd
+
+##### The answer in the book #####
+##### Runtime 93ms, Memory 16.4MB #####
 class leetcode_328:
     def oddEvenList(self, head: ListNode) -> ListNode:
         if head is None:
             return head
+		
         odd, even = head, head.next
+		even_head = head.next
 
-        while head and head.next:
-            head.next, head = head.next.next, head.next
-            
-        start = odd       
-        while start and start.next:
-            start = start.next
-        start.next = even
-        return odd
+        while even and even.next:
+            odd.next, even.next = odd.next.next, even.next.next
+			odd, even = odd.next, even.next
+			
+		odd.next = even_head
+		return head	
