b4556073ccea46eaaa29e9ddd2c3a65d79e464e9
김태수 <xotn0194@naver.com>
2021-11-05 14:07:25 +0000

    Update 2021/11/5

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
 import sys
 from collections import deque
 input = lambda : sys.stdin.readline().rstrip()
-N = int(input())
-graph, res = {}, set()
 
-for i in range(1, N+1):
-    graph[i] = int(input())
-
-def dfs(node):
-    stack = [node]
-    a_set, b_set = set(), set()
-    a_set.add(node)
-    visited = [node]   
-    while stack:
-        node = stack.pop()    
-        b_set.add(graph[node]) 
-    
-        if graph[node] not in visited:
-            stack.append(graph[node])
-            a_set.add(graph[node])
-            visited.append(graph[node])
-    
-    if a_set == b_set:
-        return a_set
-    return {}
-
-for key in graph: res.update(dfs(key))
-res = sorted(res)
-print(len(res), *res, sep = '\n')
