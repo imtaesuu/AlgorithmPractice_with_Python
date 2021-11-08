60be6a04f51aa73a3ba5e94c93bbe4fb4cd5615a
김태수 <xotn0194@naver.com>
2021-11-08 14:25:43 +0000

    Update

--- main.py
+++ main.py
@@ -2,47 +2,75 @@ import sys
 from collections import deque
 input = lambda : sys.stdin.readline().rstrip()
 
-K = int(input())
-W, H = map(int, input().split())
+graph = [] 
+block = []
+dx = [-1 ,1 ,0, 0, -1, -1, 1, 1]
+dy = [0, 0, -1, 1, 1, -1, 1, -1]
+visited = [[False]*8 for _ in range(8)]
 
-graph = []
-# graph = [[0 ,0 ,0 ,0],
-#          [-1 ,0 ,0 ,0],
-#          [0 ,0 ,-1 ,0],
-#          [0 ,-1 ,0 ,0],
-#          [0 ,-1 ,0 ,0],
-#          [0 ,-1 ,0 ,0]]
-for _ in range(H):
-    graph.append(list(map(int, input().split())))
-visited = [[[0] * (K + 1) for _ in range(W)] for _ in range(H)]
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-hdx, hdy = [-2, -1, -2, -1, 2, 1, 2, 1], [-1, -2, 1, 2, -1, -2, 1, 2]
+graph = [['.', '.', '.', '.', '.', '.', '.', '.'],
+         ['.', '.', '.', '.', '.', '.', '.', '.'],
+         ['.', '.', '.', '.', '.', '.', '.', '.'],
+         ['.', '.', '.', '.', '.', '.', '.', '.'],
+         ['#', '.', '.', '.', '.', '.', '.', '.'],
+         ['.', '#', '#', '#', '#', '#', '#', '#'],
+         ['#', '.', '.', '.', '.', '.', '.', '.'],
+         ['.', '.', '.', '.', '.', '.', '.', '.']]
+
+for i in range(8):
+    # graph.append(list(input()))
+    for j in range(8):
+        if graph[i][j] == '#':
+            block.append((i, j))
+
+
+        
+# print(*graph, sep = '\n')
+# print(block)
+
+def move_block():
+    tmp = []
+    while block:
+        x, y = block.pop()
+        visited[x][y] = False
+        if x+1 >= 8:
+            graph[x][y] = '.'
+        else:
+            graph[x][y] = '.'
+            graph[x+1][y] = '#'
+            tmp.append((x+1, y))
+    for x, y in tmp:
+        block.append((x, y))
+    print(*graph, sep = '\n')
 
 def bfs():
-    q = deque([(0, 0, 0)])
+    q = deque([(7, 0, 0)])
+    prev = 0
     while q:
-        x, y, z = q.popleft()
+        x, y, t = q.popleft()
+        
+        if t > prev and len(block) > 0:
+            print(t, prev)
+            move_block()
+            prev += 1
+        visited[x][y] = True
+        
+        if graph[x][y] == '#':
+            continue
         
         for i in range(8):
-            hx, hy = x + hdx[i], y + hdy[i]
-            if z+1>K: break
-            if 0<=hx<H and 0<=hy<W and graph[hx][hy] == 0 and visited[hx][hy][z+1] == 0:
-                visited[hx][hy][z+1] = visited[x][y][z] + 1
-                if hx == H-1 and hy == W-1:
-                    return visited[hx][hy][z+1]
-                q.append((hx, hy, z+1))
-        
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]         
-            if 0<=nx<H and 0<=ny<W and graph[nx][ny] == 0 and visited[nx][ny][z] == 0:
-                visited[nx][ny][z] = visited[x][y][z] + 1
-                if nx == H-1 and ny == W-1:
-                    return visited[nx][ny][z]
-                q.append((nx, ny, z))
-    return -1
+            nx, ny = x + dx[i], y + dy[i]
+            
+            if 0<=nx<8 and 0<=ny<8 and graph[nx][ny] != '#' and not visited[nx][ny]:
+                if nx == 0 and ny == 7:
+                    return 1
+                graph[nx][ny] = 1
+                graph[x][y] = '.'
+                q.append((nx, ny, t+1))
+    
+    return 0
 
-if H == 1 and W == 1:
-    print(0)
-else:
-    print(bfs())
-print(*visited, sep = '\n')
+print(bfs())     
+        
+        
+        
