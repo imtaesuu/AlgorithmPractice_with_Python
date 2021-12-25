3b2196fe660104a5ccee8444e9e54b4d490d3981
김태수 <xotn0194@naver.com>
2021-12-25 14:17:04 +0000

    up

--- main.py
+++ main.py
@@ -4,31 +4,58 @@ import heapq
 sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
-N = int(input())
 
-parents = list(map(int, input().split()))
-del_node = int(input())
+def dfs(root):
+    stack = [root]
+    visited = set([root])
+    
+    while stack:
+        node = stack.pop()
+        for n in graph[node]:
+            if n in visited:
+                return False
+            stack.append(n)
+            visited.add(n)
+    return True
 
-graph = [set() for _ in range(N)]
+num = 0
+while True:
+    graph = defaultdict(list)
+    nodes, childs = set(), set()
+    num += 1
+    
+    run = True
+    while run:
+        elements = list(map(int, input().split()))
+        
+        if len(elements) == 0:
+            continue
+        
+        if elements[0] == -1 and elements[1] == -1:
+            sys.exit(0)
+            
+        
+        nodes.update(elements)
+        
+        for i in range(0, len(elements), 2):
+            if elements[i] == 0 and elements[i+1] == 0:
+                run = False
+                break
+            graph[elements[i]].append(elements[i+1])
+            childs.add(elements[i+1])
+    
+    if len(graph) == 0:
+        print(f'Case {num} is a tree.')
+        continue
+    
 
-for idx in range(N):
-    if parents[idx] == -1:
-        root = idx
-    else:
-        graph[parents[idx]].add(idx)
+    root = nodes - childs
+    root.remove(0)
+
+    if len(root) != 1:
+        print(f'Case {num} is not a tree.')
+        continue
     
-def dfs(node):
-    if len(graph[node]) == 0:
-        return 1
     
-    res = 0
-    for n in graph[node]:
-        res += dfs(n)
-    
-    return res
-    
-if parents[del_node] != -1:
-    graph[parents[del_node]].remove(del_node)
-    print(dfs(root))
-else:
-    print(0)
+    print(f'Case {num} is a tree.' if dfs(root.pop()) else f'Case {num} is not a tree.')
+        
